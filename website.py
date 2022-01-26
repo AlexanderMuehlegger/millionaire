0a73aecc6402aa620e382c28a9b60246825c844f
Alexander Mühlegger <amuehlegger@tsn.at>
2022-01-26 15:44:13 +0100

    website with sessions

--- website.py
+++ website.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect
-from flask_session import Session
+from flask import Flask, render_template, request, redirect, session
 from game import game
 from app import app
+import random
+import string
 
 newApp = app(None)
 newGame = game(newApp)
@@ -11,17 +12,15 @@ newApp.setGame(newGame)
 newGame.createQuestionsArray(newGame.readFile())
 
 app = Flask(__name__)
-
-SESSION_TYPE = 'redis'
-app.config.from_object(__name__)
-Session(app)
+app.secret_key = ''.join(random.choice(string.ascii_letters) for i in range(25))
 
 question = None
 correctlyAnswered = 0
 
 @app.route("/")
 def index():
-
+    session['score'] = 0
+    session['difficulty'] = 0
     return render_template("index.html")
 
 @app.route("/game", methods=['POST', 'GET'])
@@ -36,6 +35,8 @@ def game():
         else:
             if question.rightAnswer + 1 == int(data):
                 correctlyAnswered += 1
+                currentScore = session['score']
+                currentScore += newGame.difficulty * 10
                 if newGame.difficulty < 5 and correctlyAnswered % 2 == 0:
                     newGame.difficulty += 1
                 return redirect("/game")
@@ -46,16 +47,23 @@ def game():
         return "error has occured"
     elif request.method == 'GET':
         difficulty = newGame.difficulty
+
         if difficulty >= 5:
             difficulty = "MAX"
+
+        session['difficulty'] = difficulty
+
         question = newGame.getRandomQuestion()
-        data = [question.question, question.answers[0], question.answers[1], question.answers[2], question.answers[3],
-                str(question.rightAnswer), difficulty]
+        session['question'] = question.question
+        data = [question.answers[0], question.answers[1], question.answers[2], question.answers[3]]
         return render_template('game.html', data=data)
 
 @app.route("/wrong")
 def wrong():
-    reset()
+    global correctlyAnswered
+    newGame.difficulty = 0
+    session['score'] = 0
+    correctlyAnswered = 0
     return render_template('wrong.html')
 
 @app.route("/questions")
